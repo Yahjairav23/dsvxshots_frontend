8ccdcc29e04e85dd45dbc9393a36465d8fbb1481
Yahjairav23 <vyahjaira23@gmail.com>
2020-10-19 22:07:08 -0400

    button styling

--- src/components/PortfolioButtons.js
+++ src/components/PortfolioButtons.js
@@ -3,13 +3,13 @@ import React, {Component} from 'react'
 export default class PortfolioButtons extends Component {
     render(){
         return(
-            <div>
-                <button>Birthday</button><br></br>
-                <button>Family Portait</button><br></br>
-                <button>Maternity</button><br></br>
-                <button>Newborn</button><br></br>
-                <button>Engagement</button><br></br>
-                <button>Wedding</button><br></br>
+            <div class="col-sm-4">
+                <button class="btn btn-secondary">Birthday</button><br></br>
+                <button class="btn btn-secondary">Family Portait</button><br></br>
+                <button class="btn btn-secondary">Maternity</button><br></br>
+                <button class="btn btn-secondary">Newborn</button><br></br>
+                <button class="btn btn-secondary">Engagement</button><br></br>
+                <button class="btn btn-secondary">Wedding</button><br></br>
             </div>
         )
     }
