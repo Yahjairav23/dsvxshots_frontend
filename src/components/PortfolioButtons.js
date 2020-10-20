b44ab4afb018c097f1b1b9b62f1efe33eff2102d
Yahjairav23 <vyahjaira23@gmail.com>
2020-10-19 21:50:11 -0400

    added portfolio buttons

---
+++ src/components/PortfolioButtons.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+export default class PortfolioButtons extends Component {
+    render(){
+        return(
+            <div>
+                <button>Birthday</button><br></br>
+                <button>Family Portait</button><br></br>
+                <button>Maternity</button><br></br>
+                <button>Newborn</button><br></br>
+                <button>Engagement</button><br></br>
+                <button>Wedding</button><br></br>
+            </div>
+        )
+    }
+}
--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
+import PortfolioButtons from '../components/PortfolioButtons'
 
 export default class Portfolio extends Component {
 render(){
     return(
+        <>
         <h1>Welcome to my portfolio</h1>
+        <PortfolioButtons/>
+        </>
     )
 }
 }
