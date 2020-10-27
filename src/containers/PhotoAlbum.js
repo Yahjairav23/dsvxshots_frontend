fa2639ba5398baed89c0bccaa3fad4f850bb8255
Yahjairav23 <vyahjaira23@gmail.com>
2020-10-26 22:18:32 -0400

    added photo album component

---
+++ src/containers/PhotoAlbum.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class PhotoAlbum extends Component {
+    render(){
+        return(
+            <h1>Photos</h1>
+        )
+    }
+}
+
+export default PhotoAlbum
--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 import PortfolioButtons from '../components/PortfolioButtons'
+import PhotoAlbum from './PhotoAlbum'
 
 export default class Portfolio extends Component {
+
+    let 
 render(){
     return(
         <>
         <h1>Welcome to my portfolio</h1>
         <PortfolioButtons/>
+        <PhotoAlbum/>
         </>
     )
 }
