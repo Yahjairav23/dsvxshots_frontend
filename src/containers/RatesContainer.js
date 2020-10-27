77bdf37fef8b32080fbbc03d127670728fad2e0e
Yahjairav23 <vyahjaira23@gmail.com>
2020-10-26 22:41:16 -0400

    rates

--- src/containers/RatesContainer.js
+++ src/containers/RatesContainer.js
@@ -4,6 +4,11 @@ export default class RatesContainer extends Component{
     render(){
         return(
             <h1>Here are my rates...</h1>
+            <p>Weddings</p>
+            <p>150 p/hr</p>
+
+            <p>Birthdays</p>
+            <p>$75 p/hr</p>
         )
     }
 }
