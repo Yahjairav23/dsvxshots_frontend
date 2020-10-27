fb721bb783aa06cc10e0a5c6d8ebac0ba97f60dd
Yahjairav23 <vyahjaira23@gmail.com>
2020-10-26 22:48:26 -0400

    fixed bug

--- src/containers/RatesContainer.js
+++ src/containers/RatesContainer.js
@@ -3,12 +3,14 @@ import React, { Component } from 'react'
 export default class RatesContainer extends Component{
     render(){
         return(
+            <>
             <h1>Here are my rates...</h1>
             <p>Weddings</p>
             <p>150 p/hr</p>
 
             <p>Birthdays</p>
             <p>$75 p/hr</p>
+            </>
         )
     }
 }
